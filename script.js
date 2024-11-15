7af103d70cb694acc1c9acda6389b3bc24075f73
Aleem24 <md.samiulaleem16@gmail.com>
2024-11-15 19:27:03 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Aleem";
+
+console.log(document.getElementById("string").innerHTML = name.toUpperCase());
+
+console.log((document.getElementById("s1").innerHTML = name.toLowerCase()));
+
+console.log((document.getElementById("s2").innerHTML = name.startsWith("A")));
+
